d3995c1f21b7b61a93e3c3aece79b6d4aacd8249
Tatevik Avetisyan <karotatev@gmail.com>
2023-03-28 21:33:12 +0400

    some refactoring

--- food-delivery/src/components/PopularItems/PopularItemMain/PopularItemsMain.tsx
+++ food-delivery/src/components/PopularItems/PopularItemMain/PopularItemsMain.tsx
@@ -1,19 +1,9 @@
 import PopularItemSection from "../PopularItemSection/PopularItemSection";
 import "./popularItemsMain.scss";
-import ArrowLeft from "../../../assets/svg/ArrowLeft.svg";
-import ArrowRight from "../../../assets/svg/ArrowRight.svg";
 
 const PopularItemsMain = () => {
   return (
     <div className="popular-items-main">
-      {/* <div className="paginationBtn">
-        <div>
-          <img src={ArrowLeft} alt="left" />
-        </div>
-        <div>
-          <img src={ArrowRight} alt="right" />
-        </div>
-      </div> */}
       <p className="title"> Popular items</p>
       <PopularItemSection />
     </div>
