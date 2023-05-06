b0ccde5fa8c5e62a920de820c876b383493df435
Tatevik Avetisyan <karotatev@gmail.com>
2023-05-06 23:08:53 +0400

    fixed bugs in discount % and off part

--- food-delivery/src/components/FoodTypes/FoodTypeItem/Discount/DiscountFoodItem.tsx
+++ food-delivery/src/components/FoodTypes/FoodTypeItem/Discount/DiscountFoodItem.tsx
@@ -4,10 +4,14 @@ import React from "react";
 const DiscountFoodItem: React.FC<{ discount: number }> = ({ discount }) => {
   return (
     <div className={styles.discount}>
-      <p className={styles.discountAmount}>{discount}</p>
-      <div className={styles.signs}>
-        <p className={styles.percent}>%</p>
-        <p className={styles.off}>Off</p>
+      <div className={styles.discountWrapper}>
+        <div className={styles.discountAmount}>
+          <p>{discount}</p>
+        </div>
+        <div className={styles.signs}>
+          <div className={styles.percent}>%</div>
+          <div className={styles.off}>Off</div>
+        </div>
       </div>
     </div>
   );
